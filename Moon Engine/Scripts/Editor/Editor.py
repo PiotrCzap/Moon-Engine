6f4e7be59b5b88b0a78b3e5b65e320733c970a80
PiotrCzap <piotrczapmlawa@interia.pl>
2025-10-12 22:17:20 +0200

    Adding Components

--- Moon Engine/Scripts/Editor/Editor.py
+++ Moon Engine/Scripts/Editor/Editor.py
@@ -70,6 +70,31 @@ def Editor_Window():
     )
     filesystem_label.place(x=10, y=5)
     
+    def Empty_component():
+        Empty_component_frame = ctk.CTkFrame(
+            master=Editor_hierarchy_frame,
+            width=330,
+            height=100,
+            corner_radius=0,
+            fg_color="#000000",
+            border_color="#FFFFFF",
+            border_width=2
+        )
+        Empty_component_frame.place(x=10, y=50)
+
+        Empty_component_label = ctk.CTkLabel(
+            master=Empty_component_frame,
+            text="No Component Selected",
+            text_color="white",
+            font=(Editor.Font, 20)
+        )
+        Empty_component_label.place(x=50, y=30)
+
+
+
     Editor.mainloop()
+
+    
+
 
 Editor_Window()
