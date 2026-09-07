6eb6f9c8dd68524b295194bb91d8d23e7e2072a5
PiotrCzap <piotrczapmlawa@interia.pl>
2026-09-07 18:57:54 +0200

    Added Fonts And Font Change Button

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ int main(void)
     FPS = 60;
     InitWindow(window_size_x, window_size_y, ENGINE_NAME);
     window_icon = LoadImage("src/Engine Data/icon.png");
-    font = LoadFont("src/Engine Data/fonts/moon_get-Heavy.ttf");
     SetWindowIcon(window_icon);
     SetTargetFPS(FPS);
     Engine_resource_loader();
